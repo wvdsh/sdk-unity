9daab579f570286a438efaa7e2e12320889c331d
Kevin Hu <khu14917@usc.edu>
2025-09-25 23:22:29 -0700

    feature: add ugc functions and constants

---
+++ Runtime/WavedashConstants.cs
@@ -0,0 +1,18 @@
+public static class WavedashConstants
+{
+  public static class UGCItemType
+  {
+    public const int SCREENSHOT = 0;
+    public const int VIDEO = 1;
+    public const int COMMUNITY = 2;
+    public const int GAME_MANAGED = 3;
+    public const int OTHER = 4;
+  }
+
+  public static class UGCVisibility
+  {
+    public const int PUBLIC = 0;
+    public const int FRIENDS_ONLY = 1;
+    public const int PRIVATE = 2;
+  }
+}
